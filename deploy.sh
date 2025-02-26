9dea40b32c7be41a92ebaa8fc651f55052dade07
Akash266-oss <archanagowri247@gmail.com>
2025-02-26 15:05:21 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,22 +2,16 @@
 echo running
 docker login -u akash3020  -p dckr_pat_DjcHSBpOzXX4cBcNRBwHBN25f_4
 
-if [[ $GIT_BRANCH == "dev" ]]; then
- echo "running build"
-       sh 'chmod +x build.sh'
-       sh './build.sh'
+if [[ $GIT_BRANCH == "origin/dev" ]]; then
+    # Build your project
+    sh 'chmod +x build.sh'
+    sh './build.sh'
+    docker tag test akash3020/dev
+    docker push akash3020/dev
 
-        docker tag test akash3020/dev
-        docker push akash3020/dev
-
-if [[ $GIT_BRANCH == "master" ]]; then
-        sh 'chmod +x build.sh'
-        sh './build.sh'
-
-        docker tag test akash3020/prod
-        docker push akash3020/prod
-
-else
-        echo "failed"
-fi 
-echo finish
+elif [[ $GIT_BRANCH == "origin/main" ]]; then
+    sh 'chmod +x build.sh'
+    sh './build.sh'
+    docker tag test akash3020/prod 
+    docker push akash3020/prod
+fi
